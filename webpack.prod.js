43a5b2bd72c4a2bf65ecbfbc35ed08d932057213
Guillaume Roux <guillaumeroux123@gmail.com>
2021-06-30 15:09:00 +0200

    Fix missing api key on prod (#25)

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,9 +1,11 @@
 const { merge } = require('webpack-merge');
+const webpack = require('webpack');
 const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
   mode: 'production',
   devtool: 'source-map',
+  plugins: [new webpack.EnvironmentPlugin(['MAPS_API_KEY'])],
   optimization: {
     moduleIds: 'deterministic',
     splitChunks: {
